5f6238c110aca287c8016658f4ad683fb64658fe
LeeDeuRim <mondayoff.dream@gmail.com>
2024-10-22 01:45:22 +0900

    3

--- Discord-Music/MusicSlashCommands.cs
+++ Discord-Music/MusicSlashCommands.cs
@@ -129,7 +129,7 @@ public class MusicSlashCommands : ApplicationCommandModule
     [SlashCommand("대말", "대신 말해드립니다.")]
     public async Task SayCommand(InteractionContext ctx, [Option("할말", "내용")] string content)
     {
-        await ctx.CreateResponseAsync(InteractionResponseType.DeferredChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral(true));
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder().AsEphemeral(true));
 
         await ctx.Channel.SendMessageAsync(content);
     }
